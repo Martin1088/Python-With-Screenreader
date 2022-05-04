e8db823034fc69de7f1dff8993570a072581ae0f
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2022-05-04 17:03:18 +0200

    Fehlerbehebung

--- Unterricht/Einkaufsliste.py
+++ Unterricht/Einkaufsliste.py
@@ -9,29 +9,30 @@ liste = [["Schokolade", 1, 5, 5],
 ["Mikrowelle", 150, 16, 1],
 ["Tassen", 2, 16, 40]]
 ## Ausgabe der Artikel
-print(liste[0:-1:1][0])
 print("=====")
 ## Variablen
 mwst = 0
+mwst_neu = 0
 gesamt_mwst_alt = 0
 gesamt_mwst_neu = 0
 gesamt_wert_alt = 0
 gesamt_wert_neu = 0
 for i in liste:
 	print(i[0])
-	mwst =(i[2]/100) * i[2] * i[3]
-	print("Die Mehrwertsteuer beträgt:", mwst," Euro" )
+	mwst = 0
+	mwst =(i[2]/100) * i[1] * i[3]
+	print("Die Mehrwertsteuer beträgt:", round(mwst, 2) ," Euro" )
 	gesamt_mwst_alt = gesamt_mwst_alt + mwst 
-	gesamt_wert_alt = gesamt_wert_alt + i[1] + mwst
+	gesamt_wert_alt = gesamt_wert_alt + i[1]* i[3] + mwst
 	## Neuer Betrag durch Mehrwert änderung
 	if i[2] == 5:
-		mwst =(i[2]/100) * 7 * i[3]
-		gesamt_mwst_neu = gesamt_mwst_neu + mwst 
-		gesamt_wert_neu = gesamt_wert_neu + i[1] + mwst
+		mwst_neu =(7/100) * i[1] * i[3]
+		gesamt_mwst_neu = gesamt_mwst_neu + mwst_neu 
+		gesamt_wert_neu = gesamt_wert_neu + i[1] * i[3] + mwst_neu
 	elif i[2] == 16:
-		mwst =(i[2]/100) * 19 * i[3]
-		gesamt_mwst_neu = gesamt_mwst_neu + mwst 
-		gesamt_wert_neu = gesamt_wert_neu + i[1] + mwst
+		mwst_neu =(19/100) * i[1] * i[3]
+		gesamt_mwst_neu = gesamt_mwst_neu + mwst_neu 
+		gesamt_wert_neu = gesamt_wert_neu + i[1] * i[3] + mwst_neu
 	else:
 		print("Mehrwertsteuer ist falsch")
 ## gesamte Werte 
@@ -39,5 +40,5 @@ print("Der gesamte Einkauf hat ", round(gesamt_wert_alt, 2) ," Euro gekostet")
 print("Die gesamte Mehrwertsteuer beträgt: ", round(gesamt_mwst_alt, 2), "Euro ")
 ## Nere Werte 
 print("====Neue====")
-print("Der gesamte Einkauf hat ", round(gesamt_wert_alt, 2) ," Euro gekostet")
-print("Die gesamte Mehrwertsteuer beträgt: ", round(gesamt_mwst_alt, 2), "Euro ")
+print("Der gesamte Einkauf hat ", round(gesamt_wert_neu, 2) ," Euro gekostet")
+print("Die gesamte Mehrwertsteuer beträgt: ", round(gesamt_mwst_neu, 2), "Euro ")
