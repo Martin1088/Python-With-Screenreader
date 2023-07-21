0ba897e5c1c5b094e374032be325d14d45cf1e7d
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-07-21 10:20:43 +0000

    database test mit Fehler

--- FlaskTutorial/database/app.py
+++ FlaskTutorial/database/app.py
@@ -1,9 +1,26 @@
-from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template, g
+
+import sqlite3
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+def connect_db():
+    sql = sqlite3.connect('data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
@@ -39,6 +56,9 @@ def webform():
         name = request.form['name']
         location = request.form['location']
 
+        db = get_db()
+        db.execute('insert into users (name, location) values (?, ?)', [name, location])
+        db.commit()
         #return '<h1>Hello {}. You are from {}. You have submitted the form successfully!<h1>'.format(name, location)
         return redirect(url_for('home', name=name, location=location))
 
@@ -61,6 +81,13 @@ def processjson():
     randomlist = data['randomlist']
 
     return jsonify({'result' : 'Success!', 'name' : name, 'location' : location, 'randomkeyinlist' : randomlist[1]})
+
+@app.route('/viewresults')
+def viewresults():
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+    return '<h1> The {} and name is {} from {} </h1>'.format(results[0]['id'], results[0]['name'], results[0]['location'])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
