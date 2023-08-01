dd6992815364c33be947cefcf4308bdcaa53111e
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-08-01 13:34:41 +0000

    Fehler

--- FlaskTutorial/food_webapp/app.py
+++ FlaskTutorial/food_webapp/app.py
@@ -36,14 +36,45 @@ def index():
     results_d = []
     for i in results:
         s_date = {}
+        s_date['date'] = i['entry_date']
         d = datetime.strptime( str(i['entry_date']), '%Y%m%d')
         s_date['entry_date'] = datetime.strftime(d, '%B %d, %Y')
         results_d.append(s_date)
     return render_template('home.html', results = results_d)
 
-@app.route('/view')
-def view():
-    return render_template('day.html')
+@app.route('/view/<date>', methods=['GET', 'POST'] ) # get date 20230220
+def view(date):
+    db = get_db()
+    log_cur = db.execute('select id, entry_date from log_date Where entry_date =?', [date])
+    date_result = log_cur.fetchone()
+    # insert the food with the date
+    if request.method == 'POST':
+        db.execute('insert into food_date(food_id, log_date_id) values(?,?)', [request.form['food-select'], date_result['id']])
+        db.commit()
+    d = datetime.strptime( str(date_result['entry_date']), '%Y%m%d')
+    s_date = datetime.strftime(d, '%B %d, %Y')
+    # option list data = list of foods avalible 
+    food_cur = db.execute('select id, name from food')
+    food_results = food_cur.fetchall()
+    # join on food_date get fod and log_date together
+    join_cur = db.execute('select food.name, food.protein, food.carbohydrates, food.fat, food.calories from log_date \
+            join food_date on food_date.log_date_id = log_date.id join food on food.id = food_date.food_id \
+            where log_date.entry_date = ?', [date])
+    join_results = join_cur.fetchall()
+    # make the total of the entry
+    total = {}
+    total['protein'] = 0
+    total['carbohydrates'] = 0
+    total['fat'] = 0
+    total['calories'] = 0
+    for i in join_results:
+        total['protein'] += i['protein']
+        total['carbohydrates'] += i['carbohydrates']
+        total['fat'] += i['fat']
+        total['calories'] += i['calories']
+
+    return render_template('day.html', entry_date = date_result['entry_date'], 
+            s_date = s_date, food_results = food_results, join_resuls = join_results, total = total)
 
 @app.route('/food', methods=['GET', 'POST'] )
 def food(): 
