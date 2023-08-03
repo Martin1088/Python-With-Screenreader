dbe23e6b9325f4474556ac0c7443d2c5460fc53b
martin.jurk <martin.jurk@dkfz-heidelberg.de>
2023-08-03 10:38:37 +0200

    test

---
+++ FlaskTutorial/sum_totals_per_day/.ycm_extra_conf.py
@@ -0,0 +1,15 @@
+def Settings(**kwargs):
+       return {
+           'flags': ['-x', 'python'],
+           'interpreter_path': 'python3',
+           'sys_path': [
+               '/usr/lib/python3.8',
+               # Add other paths as needed
+           ],
+           'completion': {
+               'auto_import': True,
+               'extra_imports': [
+                   'sqlite3',
+               ],
+           },
+       }
