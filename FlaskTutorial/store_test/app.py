668874fafb4b00fc9b6088aa3c8477ff918778a8
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-08-20 11:56:40 +0000

    start store

--- FlaskTutorial/store_test/app.py
+++ FlaskTutorial/store_test/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_socketio import SocketIO 
 
 
 app = Flask(__name__)
@@ -7,16 +6,5 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db-login.sqlite3' 
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@socketio.on('message')
-def receive_message(message):
-    print('Hallo {}'.format(message))
-
-if __name__ == '__main__':
-    socketio.run(app)
-    
--- FlaskTutorial/with_javascript/static/script.js
+++ FlaskTutorial/with_javascript/static/script.js
@@ -1,1 +1,6 @@
-alert('PAGE WORKS!');
+//let socket = io.connect('http://localhost:5000')
+const socket = io('http://localhost:5000')
+
+socket.on('connect' function{
+        socket.send('I am working');
+});
