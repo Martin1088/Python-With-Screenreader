1b56698fdeb1354d4159de637378ea30cdf6649c
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-08-21 09:54:36 +0000

    sote test

--- FlaskTutorial/store_test/app.py
+++ FlaskTutorial/store_test/app.py
@@ -1,54 +1,50 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage 
 
-app = Flask(__name__)
 
-photos = UploadSet('photos', IMAGES)
+db = SQLAlchemy()
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'images'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trendy.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'mysecret'
+## Product
 
-configure_uploads(app, photos)
+def create_app():
+    app = Flask(__name__)
+    #app.config.from_pyfile('config.cfg')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trendy.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'secret'
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+    @app.route('/product')
+    def product():
+        return render_template('view-product.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/cart')
+    def cart():
+        return render_template('cart.html')
 
-@app.route('/product')
-def product():
-    return render_template('view-product.html')
+    @app.route('/checkout')
+    def checkout():
+        return render_template('checkout.html')
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
+    @app.route('/admin')
+    def admin():
+        return render_template('admin/index.html', admin=True)
 
-@app.route('/checkout')
-def checkout():
-    return render_template('checkout.html')
+    @app.route('/admin/add')
+    def add():
+        return render_template('admin/add-product.html', admin=True)
 
-@app.route('/admin')
-def admin():
-    return render_template('admin/index.html', admin=True)
+    @app.route('/admin/order')
+    def order():
+        return render_template('admin/view-order.html', admin=True)
+    return app
 
-@app.route('/admin/add')
-def add():
-    return render_template('admin/add-product.html', admin=True)
 
-@app.route('/admin/order')
-def order():
-    return render_template('admin/view-order.html', admin=True)
-
-if __name__ == '__main__':
-    manager.run()
