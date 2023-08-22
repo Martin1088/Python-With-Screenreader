dd8082e3b477de3411d49a0c1676172ace94c915
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-08-22 07:06:28 +0000

    Product

--- FlaskTutorial/store_test/app.py
+++ FlaskTutorial/store_test/app.py
@@ -1,18 +1,36 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import FileStorage 
-
+from werkzeug.datastructures import FileStorage
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField
+from flask_wtf.file import FileField, FileAllowed 
 
 db = SQLAlchemy()
 
 ## Product
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True )
+    name = db.Column(db.String(50), unique=True )
+    price =  db.Column(db.Integer)
+    stock = db.Column(db.Integer)
+    description = db.Column(db.String(500))
+    image = db.Column(db.String(100))
+
+## add Product here
+class AddProduct(FlaskForm):
+    name = StringField('Name')
+    price = IntegerField('Price')
+    stock = IntegerField('Stock')
+    description = TextAreaField('Description')
+    image = FileField('Image')
+
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_pyfile('config.cfg')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trendy.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_pyfile('config.cfg')
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trendy.db'
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'secret'
     db.init_app(app)
     with app.app_context():
@@ -40,7 +58,8 @@ def create_app():
 
     @app.route('/admin/add')
     def add():
-        return render_template('admin/add-product.html', admin=True)
+        form = AddProduct()
+        return render_template('admin/add-product.html', admin=True, form = form )
 
     @app.route('/admin/order')
     def order():
