39f2cf9efcf1067276ccf79b495dac6655f563b7
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-10-04 06:37:08 +0000

    plost test

--- Basic/jupyter/table01_pandas.py
+++ Basic/jupyter/table01_pandas.py
@@ -6,7 +6,9 @@ df = pd.DataFrame(data=a)
 df = df.T
 df = df.rename(columns={0: 'BFK', 1: "PK", 2: 'GK'})
 df = df.rename(index={0: 'Mertens', 1: 'Schlütter', 2: 'Andlauer', 3: 'Fechner', 4: 'Ruhl'})
-print(df)
 df = df.drop(columns='GK')
+df = df /2
+df.BFK = df.BFK.map({1.0: 'sehr gut',  1.5: 'sehr gut', 2.0: 'gut'})
+df.PK = df.PK.map({1.0: 'sehr gut', 1.5: 'sehr gut' , 2.0: 'gut'})
 print(df)
 
