19bbd4f1a38daa2a5d07e790a0f748ff6a4d6eef
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-10-14 13:54:17 +0000

    table output

--- Basic/jupyter/table01_pandas.py
+++ Basic/jupyter/table01_pandas.py
@@ -6,14 +6,11 @@ df = pd.DataFrame(data=a)
 df = df.T
 df = df.rename(columns={0: 'BFK', 1: "PK", 2: 'GK'})
 df = df.rename(index={0: 'Mertens', 1: 'Schlütter', 2: 'Andlauer', 3: 'Fechner', 4: 'Ruhl'})
-<<<<<<< HEAD
 print(df)
 df = df.drop(columns='GK')
-=======
 df = df.drop(columns='GK')
 df = df /2
-df.BFK = df.BFK.map({1.0: 'sehr gut',  1.5: 'sehr gut', 2.0: 'gut'})
-df.PK = df.PK.map({1.0: 'sehr gut', 1.5: 'sehr gut' , 2.0: 'gut'})
->>>>>>> 24a8ef881b93602755d70b7552a2a0354d5647a8
+df.BFK = df.BFK.map({1.0: 'sehr gut',  1.5: 'gut', 2.0: 'gut'})
+df.PK = df.PK.map({1.0: 'sehr gut', 1.5: 'gut' , 2.0: 'gut'})
 print(df)
 
