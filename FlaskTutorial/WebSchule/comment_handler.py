fd2b444e70ab8056dc657e5d5c2a8ae9b0b93003
martin.jurk <martin.jurk@dkfz-heidelberg.de>
2023-10-19 19:59:52 +0200

    coments

--- FlaskTutorial/WebSchule/comment_handler.py
+++ FlaskTutorial/WebSchule/comment_handler.py
@@ -4,3 +4,26 @@ comments_blueprint = Blueprint('comments_handler', __name__)
 @comments_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+@comments_blueprint.route('/comments')
+def comment():
+
+    comments=""
+    try:
+        f = open('comments.txt', 'r')
+        f.seek(0)
+        comments = f.readlines()
+        f.close()
+    except:
+        f = open('comments.txt', 'x')
+        f.close()
+    return render_template('comments.html', comments=comments)
+
+@comments_blueprint.route('/comments', methods=['POST'])
+def comment_post():
+    comment = "\n" + request.form.get('txt')
+    f = open('comments.txt', 'a')
+    f.writelines(comment)
+    f.close()
+    return redirect(url_for('comment_handler.comment'))
+
