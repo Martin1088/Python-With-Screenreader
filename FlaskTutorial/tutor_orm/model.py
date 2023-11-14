5aadeeb7d7b2949322ae914c62e146d510804ef2
martin.jurk <martin.jurk@dkfz-heidelberg.de>
2023-11-14 09:00:38 +0100

    department

--- FlaskTutorial/tutor_orm/model.py
+++ FlaskTutorial/tutor_orm/model.py
@@ -12,3 +12,29 @@ def getdb(app):
     with app.app_context():
         db.create_all()
         return db
+
+class Department(db.Model):
+__tablename__ = 'department'
+dep_id = db.Column(db.Integer, primary_key=True, 
+autoincrement=True)
+dep_name = db.Column(db.String(100))
+dep_location = db.Column(db.String(100))
+dep_employee = db.relationship('Employee', 
+back_populates="emp_department", lazy=True)
+
+
+class Employee(db.Model):
+__tabelname__ = 'employee'
+emp_id = db.Column(db.Integer, primary_key=True, 
+autoincrement=True)
+emp_name = db.Column(db.String(100), nullable=False)
+emp_salary = db.Column(db.Integer)
+emp_dept_id = db.Column(db.Integer, 
+db.ForeignKey('department.dep_id'))
+emp_department = db.relationship('Department', 
+back_populates="dep_employee", lazy=True)
+
+
+def __init__(self, name):
+self.emp_name = name
+ 
