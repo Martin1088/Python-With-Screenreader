21112ab9b73220a039ce3acaaaeb5e3e7b840fc5
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-11-16 16:17:53 +0000

    formular redy

--- KivyTutorial/formular.py
+++ KivyTutorial/formular.py
@@ -1,21 +1,34 @@
 import kivy
-from kivy.app import App 
-from kivy.uix.label import Label 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
 
 class MyGridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(GridLayout, self).__init__(**kwargs)
-        # colums 
+        # colums
         self.cols = 2
-        # add widget 
-        self.add_widget(Label^())
+        # add widget with Label
+        self.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
+
+        self.add_widget(Label(text="Age: "))
+        self.age = TextInput(multiline=False)
+        self.add_widget(self.age)
+
 
 class MyApp(App):
     def build(self):
-        return Label(text="Here I am")
-    
-if __name__ == '__main__':
+        return MyGridLayout()
+
+
+if __name__ == "__main__":
     MyApp().run()
