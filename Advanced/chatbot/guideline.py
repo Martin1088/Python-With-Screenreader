65fbbbf30f4b25d4493d3fea9290809230008897
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-11-18 10:22:34 +0000

    api key testen

--- Advanced/chatbot/guideline.py
+++ Advanced/chatbot/guideline.py
@@ -1,19 +1,22 @@
-import openai
 import os
 
-from dotenv import load_dotenv, find_dotenv
+import openai
+from dotenv import find_dotenv, load_dotenv
+
 _ = load_dotenv(find_dotenv())
 
-openai.api_key  = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
+        temperature=0,  # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
+
 
 text = f"""
 You should express what you want a model to do by \ 
