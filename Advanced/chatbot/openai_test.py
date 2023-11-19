5f3d9304b41b77525e81ae11e08d62fcb06a366e
martin.jurk <martin.jurk@dkfz-heidelberg.de>
2023-11-19 14:39:58 +0100

    test poem

--- Advanced/chatbot/openai_test.py
+++ Advanced/chatbot/openai_test.py
@@ -9,3 +9,14 @@ _ = load_dotenv(find_dotenv())
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 # api_secret = os.getenv("")
 # client = OpenAI
+
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
